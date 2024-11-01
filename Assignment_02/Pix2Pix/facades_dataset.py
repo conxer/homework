b097f82a491e8b08fbb4e774cf073854ac8871a2
conxer <167391954+conxer@users.noreply.github.com>
2024-11-01 19:44:53 +0800

    Update facades_dataset.py

--- Assignment_02/Pix2Pix/facades_dataset.py
+++ Assignment_02/Pix2Pix/facades_dataset.py
@@ -14,10 +14,11 @@ class FacadesDataset(Dataset):
         img_name = self.image_filenames[idx]
         img_color_semantic = cv2.imread(img_name)
         img_color_semantic = cv2.cvtColor(img_color_semantic, cv2.COLOR_BGR2RGB)  # 转换为 RGB 格式
+        img_color_semantic = cv2.resize(img_color_semantic, (1024, 512))
         image = torch.from_numpy(img_color_semantic).permute(2, 0, 1).float() / 255.0 * 2.0 - 1.0
         
-        # 根据 1200x600 分辨率切分
-        image_rgb = image[:, :, :600]  # 前 600 列
-        image_semantic = image[:, :, 600:]  # 后 600 列
+        # 根据 1024x512 分辨率切分
+        image_rgb = image[:, :, :512]  # 前 512 列
+        image_semantic = image[:, :, 512:]  # 后 512 列
         
         return image_rgb, image_semantic
